3dd656eef7c6b65ccf4822e96310c54cabfe616c
Kévin Ung <kung@nx-domain.lan>
2020-02-12 14:16:33 +0100

    First commit

---
+++ src/test/java/squash/tfauto/CalcFailure.java
@@ -0,0 +1,16 @@
+package squash.tfauto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalcFailure {
+
+    @Test
+    @DisplayName("Substraction")
+    public void subFailure(){
+        int a = 5;
+        int b = 10;
+        Assertions.assertTrue((b-a)==0, "Le résultat du calcul est incorrect. " + b + " - " + a + " n'est pas égal à 0");
+    }
+}
