21cc78a63649d148881272ad1a105fbb5ef4d891
UKevin94 <kevinung94@gmail.com>
2021-01-28 12:25:40 +0100

    Added new cases to check both with and without displayName on methods and classes

--- src/test/java/squash/tfauto/CalcNoDNTest.java
+++ src/test/java/squash/tfauto/CalcNoDNTest.java
@@ -4,13 +4,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class CalcFailureTest {
+public class CalcNoDNTest {
 
     @Test
     @DisplayName("Substraction")
+    public void subDNFailure(){
+        int a = 5;
+        int b = 10;
+        Assertions.assertTrue((b-a)==0, "Le résultat du calcul est incorrect. " + b + " - " + a + " n'est pas égal à 0");
+    }
+
+    @Test
     public void subFailure(){
         int a = 5;
         int b = 10;
         Assertions.assertTrue((b-a)==0, "Le résultat du calcul est incorrect. " + b + " - " + a + " n'est pas égal à 0");
     }
+
+
 }
--- src/test/java/squash/tfauto/CalculatorTest.java
+++ src/test/java/squash/tfauto/CalculatorTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Calculator")
 public class CalculatorTest {
 
     @Test
-    @DisplayName("Addition")
     public void addSuccess(){
         int a = 5;
         int b = 12;
@@ -15,7 +15,23 @@ public class CalculatorTest {
     }
 
     @Test
+    @DisplayName("Addition")
+    public void addDNSuccess(){
+        int a = 5;
+        int b = 12;
+        Assertions.assertTrue((a+b)==17, "Le résultat du calcul est incorrect. " + a + " * " + b + " n'est pas égal à 17");
+    }
+
+    @Test
     public void multFailure(){
+        int first = 2;
+        int second = 4;
+        Assertions.assertTrue((first*second)==6, "Le résultat du calcul est incorrect. " + first + " * " + second + " n'est pas égal à 6");
+    }
+
+    @Test
+    @DisplayName("Addition")
+    public void multDNFailure(){
         int first = 2;
         int second = 4;
         Assertions.assertTrue((first*second)==6, "Le résultat du calcul est incorrect. " + first + " * " + second + " n'est pas égal à 6");
