501fa6ec93cde3bdb8f2c0b39e678c58735773d0
kungnx <kung@henix.fr>
2022-04-21 10:46:05 +0200

    new branch to try another pom

--- src/test/java/squash/tfauto/CalculatorTest.java
+++ src/test/java/squash/tfauto/CalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+//@RunWith(org.junit.platform.runner.JUnitPlatform.class)
 public class CalculatorTest {
 
     @Test
