7cf3c0c0c9cb7b46c389ce70a6916b8a2eb5a118
Trevor <80819010+tru3v0r@users.noreply.github.com>
2022-08-16 09:59:08 +0200

    Change CalculatorTest @DisplayName

--- src/test/java/squash/tfauto/CalculatorTest.java
+++ src/test/java/squash/tfauto/CalculatorTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Calculator")
+@DisplayName("CalculatorDisplayName")
 public class CalculatorTest {
 
     @Test
